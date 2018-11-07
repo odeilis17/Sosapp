40d9f21ac860f94c2c43c89f58bd71b64f754b0a
jonas <odeilis1717@gmail.com>
2018-11-06 23:38:38 -0300

    Primera version de Sosapp

---
+++ app/src/main/java/ado/edu/itla/sosapp/MainActivity.java
@@ -0,0 +1,16 @@
+package ado.edu.itla.sosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+  private static final String TAG = "SOSAPP.MAINACTIVITY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d (TAG, "Entrando a Main Activity");
+        Log.d (TAG, "Esto es un error");
+    }
+}
